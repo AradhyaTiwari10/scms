fb1dd523630d9e41c82c58a6bfff09a1e9187eed
AradhyaTiwari10 <aradhyafcb@gmail.com>
2026-04-08 03:32:02 +0530

    feat: add protected profile route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const { signup, login } = require("../controllers/authController");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 router.post("/signup", signup);
 router.post("/login", login);
+
+router.get("/profile", authMiddleware, (req, res) => {
+  res.json({
+    message: "Protected route accessed",
+    user: req.user,
+  });
+});
 
 module.exports = router;
