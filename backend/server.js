9ffefd6469d7ec1cd36c74e6fdda9b5b92f87a2e
AradhyaTiwari10 <aradhyafcb@gmail.com>
2026-04-07 23:27:53 +0530

    feat: connect test route to server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const testRoutes = require("./routes/testRoutes");
 
 dotenv.config();
 connectDB();
@@ -10,6 +11,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api", testRoutes);
 
 app.get("/", (req, res) => {
   res.send("SCMS Backend Running");
